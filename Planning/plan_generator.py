f2db18dafec4c84abfa1e1f9863e72bc2baf617c
Caramel <octavio.delser@gmail.com>
2020-12-20 23:33:53 +0100

    working on parser

--- Planning/plan_generator.py
+++ Planning/plan_generator.py
@@ -1,6 +1,7 @@
 from os import listdir
 from os.path import join
 
+from Planning.pddl_lib.pddlpy.domain_problem import DomainProblem
 from Planning.pddl_parser.planner import Planner
 
 # print(p)
@@ -12,7 +13,7 @@ planner = Planner()
 
 
 def get_plan(domain_path, p_path):
-    return planner.solve(domain_path, p_path)
+    return DomainProblem(domain_path, p_path)
 
 
 def get_plans(domain_path, p_dir):
@@ -25,4 +26,39 @@ def get_plans(domain_path, p_dir):
 
 plans = get_plans('depotsstrips/Depots.pddl', 'depotsstrips/pfiles')
 p = plans[0]
+
+action_keys = p.operators()
+state = p.initialstate()
+
+
+class InvalidType(Exception):
+    pass
+
+
+def perform_action(action_key, action_variables, state):
+    """
+        o.variable_list
+        {'?x': 'Truck', '?y': 'Place', '?z': 'Place'}
+        o.precondition_neg
+        set()
+        o.precondition_pos
+        {('at', '?x', '?y')}
+        o.effect_neg
+        {('at', '?x', '?y')}
+        o.effect_pos
+        {('at', '?x', '?z')}
+    """
+    action = p.domain.operators[action_key]
+    type_dict = action.variable_list
+    for index, key in enumerate(type_dict.keys()):
+        if not type_dict[key] == p.worldobjects()[action_variables[index]]:
+            raise InvalidType(action_variables, type_dict)
+    return state
+
+
+perform_action("Drive", ('truck2', 'depot0', 'depot1'), state)
+
+print('=========world obj=======')
+print(list(p.worldobjects()))
+p._typesymbols()
 print(p)

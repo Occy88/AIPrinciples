337ea1bf4eab9a99e076b10577ed2990d08616ad
caramel <octavio.delser@gmail.com>
2021-01-21 12:56:56 +0000

    parse for ff solver completed

--- Planning/project/controller.py
+++ Planning/project/controller.py
@@ -1,9 +1,13 @@
 from Planning.project.generator.generator import Generator
 from Planning.project.solver.solver import Solver
+from Planning.project.parser.ff_to_action import FFToActionParser
 
 # generator
 pddl = 'blocksworld'
 # g = Generator()
 # g.gen_problem_states(pddl, 100)
-s = Solver()
-s.solve_generated_states(pddl)
+# s = Solver()
+# s.solve_generated_states(pddl)
+
+p = FFToActionParser()
+p.parse_all(pddl)

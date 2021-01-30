8c4780024117c45a3224c9248921163475c6424f
caramel <octavio.delser@gmail.com>
2021-01-30 21:04:01 +0000

    convered parsed output object to simple datastructure ready for use

--- Planning/project/problem_convert/pddl_parser.py
+++ Planning/project/problem_convert/pddl_parser.py
@@ -3,28 +3,63 @@ import sys
 from lark import Lark, Transformer, v_args
 
 
-# class PddlToJson(Transformer):
-#     @v_args(inline=True)
-#     def string(self, s):
-#         return s[1:-1].replace('\\"', '"')
-#
-#     array = list
-#     pair = tuple
-#     object = dict
-#     number = v_args(inline=True)(float)
-#
-#     null = lambda self, _: None
-#     true = lambda self, _: True
-#     false = lambda self, _: False
+class PddlToJson(Transformer):
+    def pddl(self, args):
+        domain = args[0]
+        predicates = args[1]
+        actions = args[2:]
+        return {'domain': args[0], 'predicates': predicates, 'actions': actions}
+
+    def string(self, s):
+        return s
+
+    def predicates(self, p_list):
+        return p_list
+
+    def predicate(self, args):
+        name = args[0][0].value
+        vals = frozenset(tuple(args[1:]))
+        p = frozenset({name, vals})
+        return p
+
+    def var(self, v):
+        val = v[0][0].value
+        return val
+
+    def parameters(self, args):
+        return frozenset(tuple(args))
+
+    def definition(self, name):
+        return name[0][0].value
+
+    def action(self, args):
+        name, parameters, precondition, effect = args
+        return {'name': name[0].value, 'params': parameters, 'pre': precondition, 'effect': effect}
+
+    def effect(self, p_set):
+        negative = set()
+        positive = set()
+        for p in p_set:
+            try:
+                if p.data == 'n_predicate':
+                    negative.add(frozenset(p.children))
+            except Exception as e:
+                positive.add(p)
+            print(p)
+        return {'positive': positive, 'negative': negative}
+
+    precondition = set
+    number = v_args(inline=True)(float)
 
 
 sample_conf = open('./blocksworld.pddl', 'r').read()
 grammar = open("./grammar", 'r').read()
 print(sample_conf)
 print(grammar)
-parser = Lark(grammar, parser="lalr")
+parser = Lark(grammar, transformer=PddlToJson(), parser="lalr")
+parsed = parser.parse(sample_conf)
 
-print(parser.parse(sample_conf).pretty())
+print(parser.parse(sample_conf))
 
 # #
 # # $name

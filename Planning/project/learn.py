b4b847d4f4d2a55a413cee9bfe5b2d680bcd069e
occy88 <octavio.delser@gmail.com>
2021-03-03 01:12:23 +0100

    working towards learning and a fix with python library

--- Planning/project/learn.py
+++ Planning/project/learn.py
@@ -1,11 +1,22 @@
 from pracmln import MLN
+results=open("results",'w')
 # state.perform_action('move-b-to-t', ('b9', 'b4'))
 mln_params = 'mln_params.mln'
-mln_database = 'mln_db.mln'
+mln_database = 'mln_db_r.mln'
 logic = 'FirstOrderLogic'
 grammar = 'StandardGrammar'
 method = 'pseudo-log-likelihood'
 mln = MLN(logic, grammar, mln_params)
 from pracmln import Database
+import numpy as np
+# print(np.exp(10),np.exp(100),np.exp(1000),np.exp(100000000000))
+class pr():
+    def write(self,*args,**kwargs):
+        print(args,kwargs)
+mln.write(pr())
+mln.write(results)
 db = Database.load(mln, mln_database)
 mln.learn(db)
+mln.write(pr())
+
+mln.write(results)
